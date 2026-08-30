374620f65a46ed6919672ff77fdb0964401981e9
ProAi-0121 <harshsonawane0121@gmail.com>
2026-08-30 18:33:58 +0530

    e

--- main.py
+++ main.py
@@ -541,7 +541,7 @@ def get_users():
             latest = None
             if history:
                 try:
-                    latest = history[-1]
+                    latest = history[0]
                 except Exception:
                     latest = None
             if not latest:
